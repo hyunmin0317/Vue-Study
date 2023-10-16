644333d22df50563425bf21098dbf9f05c195487
hyunmin0317 <choihm9903@naver.com>
2023-10-16 19:21:01 +0900

    feat: 시스템 메뉴 작성 (#7)

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -1,7 +1,17 @@
 <template>
     <header>
         <div class="topmenu">
-            <div class="contentbox"></div>
+            <div class="contentbox">
+                <div class="logo">
+                    <button>
+                        <img src="/images/logo.png" alt="이지스퍼블리싱 로고" />
+                    </button>
+                </div>
+                <div class="system">
+                    <button class="login">로그인</button>
+                    <button class="member">회원가입</button>
+                </div>
+            </div>
         </div>
         <nav>
             <div class="contentbox"></div>
