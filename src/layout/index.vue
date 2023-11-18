cc3f5717a326d047340c15e05453649bf7f5a074
hyunmin0317 <choihm9903@naver.com>
2023-11-18 14:28:10 +0900

    feat: 푸터 영역 만들기 (#7)

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,12 +2,10 @@
     <section id="wrap">
         <h1 class="blind">웹사이트 제목</h1>
         <Header />
-        <section>상단 영역</section>
         <section id="container">
             <router-view />
         </section>
         <Footer />
-        <section>하단 영역</section>
     </section>
 </template>
 
